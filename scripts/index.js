3ab648589c3c1091c7bc52438ea1ceb7d41bdb4a
Kate Timofeeva <timofeeva.katerina2016@yandex.ru>
2024-10-15 23:59:20 +0300

    первый вариант

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
-// @todo: Темплейт карточки
+const cardPlaces = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
+const closeButton = document.querySelector('.popup__close');
+const saveButton = document.querySelector('.popup__button')
+const newCardPopup = document.querySelector('.popup_type_new-card');
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция создания карточки
+function addCard(placeName, placeLink) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    cardElement.querySelector('.card__image').setAttribute('src', placeLink);
+    cardElement.querySelector('.card__image').setAttribute('alt', placeName);
+  
+    cardElement.querySelector('.card__title').textContent = placeName;
+  
+    cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('card__like-button_is-active');
+    });
+
+    deleteCard(cardElement);
+
+    return cardElement;
+  }
 
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function () {
+        cardElement.remove();
+    });
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (element) {
+    const cardElement = addCard(element.name, element.link);
+    cardPlaces.append(cardElement);
+});
